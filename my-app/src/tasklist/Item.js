7f2f5ee9cf34906705acdbad2c274ac852dcb0a7
lucaspk <jobson.silva@ccc.ufcg.edu.br>
2019-07-06 22:13:24 -0300

    refactoring do tasktodoinfo

--- my-app/src/tasklist/Item.js
+++ my-app/src/tasklist/Item.js
@@ -2,13 +2,25 @@ import React from 'react'
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import {CustomColorCheckbox} from './taskPriority/CustomColorCheckbox'
+import InputBase from '@material-ui/core/InputBase';
+
+// item com nome editavel todolist
+// editavel []
+// att o nome ds que armazena a lista de tarefas []
+// att no db []
+const testandoSom = (text) => {
+  return <InputBase id="task-name"
+            defaultValue={text}
+            inputProps={{ 'aria-label': 'naked' }}
+          />
+}
 
 export const Item = (props) => {
     const text = props.taskname
     return (<FormGroup>
               <FormControlLabel
                 control={CustomColorCheckbox(props)}
-                label={text}
+                label={testandoSom(text)}
               /> 
             </FormGroup>);
   }
--- my-app/src/tasklist/TaskTodoInfo.js
+++ my-app/src/tasklist/TaskTodoInfo.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
+const defaultTodoMsg = (props) => {
+    return <h2>You have {props.len} tasks to complete.</h2>
+}
+
+const noTasksTodo = () => {
+    return <h2>You have no tasks to complete. Enjoy your day!</h2>
+}
+
+const oneTaskTodo = () => {
+    return <h2>You have 1 task to complete.</h2>
+}
+
 export const TaskTodoInfo = (props) => {
-    let msg = <h2>You have {props.len} tasks to complete.</h2>
+    let msg = defaultTodoMsg(props)
     if (props.len === 0) {
-        msg = <h2>You have no tasks. Enjoy your day!</h2>
+        msg = noTasksTodo()
     } else if (props.len === 1) {
-        msg = <h2>You have {props.len} task to complete.</h2>
+        msg = oneTaskTodo()
     }
     return msg
 }
